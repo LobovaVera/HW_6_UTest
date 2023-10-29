6aaf39ac7f7290f0abf9d3553f1f80518c746831
Vera <pchilli@yandex.ru>
2023-10-29 14:52:44 +0300

    Initial commit

---
+++ src/test/java/org/example/ViewTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ViewTest{
+View view = new View();
+
+@Test
+        void enterListTest() {
+                int[] expected = {1,2,5,8}; // Создаем эталонный лист с целочисленными значениями
+                ByteArrayInputStream in = new ByteArrayInputStream("1 2 5 8\n".getBytes()); // В поток ввода передаем 1 2 3
+                System.setIn(in);
+                int[] actual = view.enterList();  // Заполняем лист используя метод enterList тестируемого класса
+
+                assertArrayEquals(expected,actual,"неверное заполнение листа");
+                System.setIn(System.in); // Возвращаем поток ввода в исходное состояние
+                }
+
+
+@Test
+        void printAverage() {
+        }
+}
